713c37a8864a9d2dd74d6ba5f05742b540ad3093
Fullstack web developer <84002031+ricardo-applepie@users.noreply.github.com>
2022-01-16 02:15:53 +0100

    Update PopUPChatWindow.jsx

--- src/components/popUpChatWindow/PopUPChatWindow.jsx
+++ src/components/popUpChatWindow/PopUPChatWindow.jsx
@@ -6,12 +6,10 @@ function PopUpChatWindow() {
     <div className="chat-popup" id="myForm">
       <form action="/action_page.php" className="form-container">
         <h1>Chat</h1>
-
         <label htmlFor="msg">
           <b>Message</b>
         </label>
         <textarea placeholder="Type message.." name="msg" required></textarea>
-
         <button type="submit" className="btn">
           Send
         </button>
